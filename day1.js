83744598c3d428e9e0a0e5608d1d46047af77372
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-11-13 16:30:39 +0530

    Added Js

---
+++ day1.js
@@ -0,0 +1,43 @@
+// let a = 7
+// let b = 9
+// console.log(a + b)
+
+// num1=78
+// 5num2=89
+// aman=90
+// amankumar=90
+// amankumar=90
+// aman = 90
+// AMana = 65
+// console.log(aman)
+// console.log(AMana)
+// let b; // declartion
+// b = 67 // initilization
+// let a = 78; // declaration with initilization
+
+// This is my single line comments
+// let a, b, c;
+// a = 89
+// b = 45
+/*
+console.log(a)
+console.log(b)
+// vdjivk
+// knsdklcvndsk
+// khik
+*/
+// let amn_________a$_ = 89
+// console.log(amn_________a$_)
+
+// const a = 90;
+// let b = 85
+// var f1 = 89
+// var f1 = 56
+// f1 = 59
+// console.log(f1)
+
+// console.log(a)
+// a = 23
+// b = 34
+// console.log(a)
+// console.log(b)
