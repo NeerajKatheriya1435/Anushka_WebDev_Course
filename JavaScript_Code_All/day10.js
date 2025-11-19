4977f9b2a131ed3ca25b8c09c0028e20cedb8d08
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-11-19 12:21:31 +0530

    import and modlues

---
+++ JavaScript_Code_All/day10.js
@@ -0,0 +1,15 @@
+// console.log("Hello")
+
+// export const greet = () => {
+//     console.log("Hello I am a Modules im good boy")
+// }
+
+// export const obj1 = {
+//     name1: "Anamika",
+//     age: 78,
+//     num1: 9,
+//     num2: 6,
+//     addtWoNum: function () { console.log(`The sum is: ${this.num1 + this.num2}`) }
+// }
+
+// obj1.addtWoNum(6, 4)
---
+++ JavaScript_Code_All/day9.js
@@ -0,0 +1,62 @@
+// import math from "math"
+
+// console.log(Math.floor(5.89))
+// console.log(Math.ceil(5.89))
+// console.log(Math.sqrt(25))
+// console.log(Math.cbrt(27))
+// console.log(Math.pow(4, 3))
+// console.log(Math.log2(32))
+// console.log(Math.PI)
+// console.log(Math.E)
+// console.log(Math.cos(Math.PI / 4))
+// // console.log(Math.cos(Math.PI / 4))
+// console.log(1 / Math.sqrt(2))
+// console.log(1 / Math.sqrt(3)) // .404
+// console.log(Math.sqrt(3))
+// console.log(Math.sqrt(3))
+// console.log(Math.sqrt(2))
+// console.log(Math.round(4.67))
+// console.log(Math.round(4.27))
+// console.log(Math.trunc(3.27))
+// console.log(Math.sign(-90))
+// console.log(Math.abs(-90))
+// console.log(Math.min(3, 5))
+// console.log(Math.max(13, 5))
+// console.log(Math.ceil(10000 * Math.random()))
+
+// import { greet } from "./day10.js";
+
+// greet()
+// import { obj1 } from "./day10.js";
+// console.log(obj1.age)
+// obj1.addtWoNum()
+
+// Install to node.js to run directly javscript
+// react-native snippet
+// code runner
+// run in terminal change setting
+
+// console.log("Hello i am good")
+// timedId = setTimeout(() => {
+//     console.log("Internal Settimeout")
+// }, 3000);
+// clearInterval(timedId)
+
+// // console.log("After Setimeout")
+// timeid = setInterval(() => {
+//     console.log("Hello anushka")
+// }, 800);
+
+// setTimeout(() => {
+//     console.log("terminated")
+//     clearInterval(timeid)
+// }, 3000);
+
+// let a = 90
+// const greet = () => {
+//     a = 45
+//     console.log(a)
+// }
+// greet()
+// console.log("After Greet")
+// console.log(a)
