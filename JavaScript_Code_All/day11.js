b46ab2c9eb9d39867be8224f68e6c03c56975c44
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-11-21 11:10:36 +0530

    Added Files with notes

---
+++ JavaScript_Code_All/day11.js
@@ -0,0 +1,38 @@
+// let age = 89
+// const name1 = "shubham"
+
+// let str1 = `My name is ${name1} and age is: ${age}`
+
+// console.log(str1)
+
+// str1 = "my name is\tneeraj \nmy age is 89"
+// console.log(str1)
+
+// let lis1 =
+
+// let [a, , c, d = 8] = [3, 6, 9]
+// console.log(a)
+// console.log(c)
+// console.log(d)
+// let arr1 = [6, 3, 5, 67, 78, 8]
+
+// let [a, b, ...an1] = arr1
+// console.log(a)
+// console.log(b)
+// console.log(an1)
+
+// let arr1 = [5, 7, 34, 56, 7]
+// let arr2 = [4, 7, 3, 6, ...arr1]
+// console.log(arr2)
+
+// function sum(...arr1) {
+//     // console.log(arr1)
+//     let add = 0
+//     for (item of arr1) {
+//         add += item
+//     }
+//     console.log(add)
+// }
+
+// sum(2, 5, 4, 7, 2, 4, 5)
+// console.warn("Hello")
